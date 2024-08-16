a1fe6a4d856fabb93dbe4c408f38b486d100fafc
Fontalvojs <mejia_andres@hotmail.es>
2024-08-16 00:59:17 -0500

    se ajustó el dominio para proteger de cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://www.la-tribuna-front.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
